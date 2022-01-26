b7ad3c972cb79546f41998a31e67700e289d447c
Brad Brookfield <bradbrookfield1@gmail.com>
2022-01-25 23:49:17 -0600

    Almost done with plots

--- Data Visualization/Mic.m
+++ Data Visualization/Mic.m
@@ -3,17 +3,15 @@ classdef Mic
     properties
         % Required inputs:
         % 1. Name for mic
-        % 2. .mat FR voltage file
+        % 2. .mat FR and PSD voltage file
         % 3. Frequency range to be covered
         % 4. Number of elements in the tested frequency range
-        % 5. .mat PSD voltage file
-        % 6. Sampling frequency
-        % 7. Microphone sensitivity (optional)
+        % 5. Sampling frequency
+        % 6. Microphone sensitivity (optional)
         name
-        micVoltFile
+        dataFile
         freqRange
         numFreqs
-        psdFile
         sampFreq
         micSensDBV % STILL TO BE IMPLEMENTED...
 
@@ -23,6 +21,7 @@ classdef Mic
         lvlSensVolt
         logFreqArray
         linFreqArray
+        freqArray
         lvlDBsplFR
         lvlDBsplPSD
         lvlVoltFR
@@ -39,7 +38,7 @@ classdef Mic
     end
 
     methods
-        function obj = Mic(name, micVoltFile, freqRange, numFreqs, psdFile, sampFreq, micSensDBV)
+        function obj = Mic(name, dataFile, freqRange, numFreqs, sampFreq, micSensDBV)
             % This constructor takes in required voltage data and a few
             % other details, and then automatically calculates
             % everything needed and plots the frequency response (FR), the power
@@ -48,10 +47,9 @@ classdef Mic
 
             % Required inputs:
             obj.name = name;
-            obj.micVoltFile = micVoltFile;
+            obj.dataFile = dataFile;
             obj.freqRange = freqRange;
             obj.numFreqs = numFreqs;
-            obj.psdFile = psdFile;
             obj.sampFreq = sampFreq;
 
             % Optional: include mic sensitivity.
@@ -67,34 +65,31 @@ classdef Mic
 
             % COME READ ME!!! https://acousticnature.com/journal/which-microphone-sensitivity-is-better-dbv-vs-mv
 
-%             obj.lvlDBsplFR = lvlArrayAvgDB(obj);
+%             [obj.lvlDBsplFR, obj.lvlDBsplPSD] = lvlArrayAvgDB(obj);
             % FIX ME!!! FAKE DATA FOR TESTING!!!
             obj.lvlDBsplFR = [56.4, 61.7, 72.5, 80.1, 88, 90.1, 87.2, 87, 82.9, 80.8, 80.4, 79, ...
                 82, 80.3, 84.5, 82, 89, 87.4, 88.2, 86.3, 87.3, 85.7, 85.3, 88.6, 87];
-
-%             obj.lvlDBsplFR = lvlArrayAvgDB(obj);
             % FIX ME!!! FAKE DATA FOR TESTING!!!
             obj.lvlDBsplPSD = [56.4, 61.7, 72.5, 80.1, 88, 90.1, 87.2, 87, 82.9, 80.8, 80.4, 79, ...
                 82, 80.3, 84.5, 82, 89, 87.4, 88.2, 86.3, 87.3, 85.7, 85.3, 88.6, 87];
 
             % Convert sound pressure dB to voltage:
             % https://electronics.stackexchange.com/questions/96205/how-to-convert-volts-to-db-spl
-            obj.lvlVoltFR = dbsplToVolt(obj, 'Log');
-            obj.lvlVoltPSD = dbsplToVolt(obj, 'Lin');
+            [obj.lvlVoltFR, obj.lvlVoltPSD] = dbsplToVolt(obj);
 
-%             obj.micVoltFR = avgMicVolt(obj, 'Log');
-%             obj.micVoltPSD = avgMicVolt(obj, 'Lin');
+%             [obj.micVoltFR, obj.micVoltPSD] = avgMicVolt(obj);
             % FIX ME!!! FAKE DATA FOR TESTING!!!
             obj.freqArray = obj.logFreqArray(2:24);
-            volt = avgMicVolt(obj, 'Log');
+            [volt, ~] = avgMicVolt(obj);
             obj.micVoltFR = [9e-5, volt, 0.0015];
 
-            % Normalize two data sets via voltage gain ratio.
+            % Normalize the data sets.
             obj.normVoltFR = obj.micVoltFR./obj.lvlVoltFR;
+            obj.normVoltPSD = obj.micVoltPSD./obj.lvlVoltPSD;
             % Convert to dB.
             obj.normDBFR = 20*log10(obj.normVoltFR);
             % Find the mean gain, the standard deviation, and the
-            % corresponding tolerance percentage.
+            % corresponding tolerance percentage for FR.
             obj.meanGain = sum(obj.normDBFR)/length(obj.normDBFR);
             obj.stdDev = sqrt((sum(obj.normDBFR.^2)/length(obj.normDBFR)) - (obj.meanGain^2));
             obj.tolerance = (((obj.meanGain - obj.stdDev) - obj.meanGain)*100)/obj.meanGain;
@@ -110,17 +105,17 @@ classdef Mic
         function plotFreqResp(obj)
             % Plots FR graphs for the mic, the level meter, and then the
             % normalized response.
-            subplot(2, 3, 1);
+            subplot(2, 4, 1);
             semilogx(obj.logFreqArray, obj.micVoltFR); grid on;
             xlabel('Frequency (Hz)'); ylabel('Voltage (V)');
             title('Mic Voltage Data');
         
-            subplot(2, 3, 2);
+            subplot(2, 4, 2);
             semilogx(obj.logFreqArray, obj.lvlVoltFR); grid on;
             xlabel('Frequency (Hz)'); ylabel('Voltage (V)');
             title('Level Meter Voltage');
         
-            subplot(2, 3, 3);
+            subplot(2, 4, 3);
             semilogx(obj.logFreqArray, obj.normDBFR); grid on; ylim([-80, 0]);
             xlabel('Frequency (Hz)'); ylabel('Normalized Mic Voltage Gain (dB)');
             title('True Frequency Response');
@@ -129,7 +124,7 @@ classdef Mic
         function dispFreqRespDetails(obj)
             % Displays the mic FR mean gain, standard deviation, and
             % tolerance on the figure.
-            ax = subplot(2, 3, 6);
+            ax = subplot(2, 4, 4);
             title('True Frequency Response Details');
             str1 = sprintf('Mean Normalized Gain: %0.2f dB', obj.meanGain);
             str2 = sprintf('Standard Deviation: +-%0.2f dB', obj.stdDev);
@@ -149,96 +144,93 @@ classdef Mic
             % Plots PSD graphs for the mic, the level meter, and then the
             % normalized response.
 
-%             load(obj.psdFile);
-%             [psd, freqs] = periodogram(micPSDVoltWhite, [], [], obj.sampFreq);
-%             subplot(2, 3, 4);
-%             plot(freqs, 10*log10(psd));
-%             xlabel('Hz'); ylabel('dB/Hz');
-%             title('Power Spectral Density (White Noise)');
-% 
-%             [psd, freqs] = periodogram(micPSDVoltSine, [], [], obj.sampFreq);
-%             subplot(2, 3, 5);
-%             plot(freqs, 10*log10(psd));
-%             xlabel('Hz'); ylabel('dB/Hz');
-%             title('Power Spectral Density (Sine Wave)');
-
-            subplot(2, 3, 5);
-            t = 0:1/obj.sampFreq:1-1/obj.sampFreq;
-            x = cos(2*pi*100*t) + sin(2*pi*150*t) + randn(size(t));
-            [pxx, f] = periodogram(x, rectwin(length(x)), length(x), obj.sampFreq);
-            plot(f, 10*log10(pxx));
+            voltMicPSD = buildPSDSig(obj, obj.micVoltPSD);
+            voltLvlPSD = buildPSDSig(obj, obj.lvlVoltPSD);
+            voltNormPSD = buildPSDSig(obj, obj.normVoltPSD);
+            [pMic, fMic] = periodogram(voltMicPSD, rectwin(length(voltMicPSD)), length(voltMicPSD), obj.sampFreq);
+            [pLvl, fLvl] = periodogram(voltLvlPSD, rectwin(length(voltLvlPSD)), length(voltLvlPSD), obj.sampFreq);
+            [pNorm, fNorm] = periodogram(voltNormPSD, rectwin(length(voltNormPSD)), length(voltNormPSD), obj.sampFreq);
+            subplot(2, 4, 5);
+            plot(fMic, 10*log10(pMic));
             xlabel('Hz'); ylabel('dB/Hz');
-            title('Power Spectral Density');
-
-%             subplot(2, 3, 5);
-%             t = 0:1/obj.sampFreq:1-1/obj.sampFreq;
-% 
-%             % Build the signal into its frequency components.
-%             for i = 1:length(x)
-% 
-% 
-%             x = cos(2*pi*100*t) + sin(2*pi*150*t) + randn(size(t));
-%             [pxx, f] = periodogram(x, rectwin(length(x)), length(x), obj.sampFreq);
-%             plot(f, 10*log10(pxx));
-%             xlabel('Hz'); ylabel('dB/Hz');
-%             title('Power Spectral Density');
+            title('Mic Power Spectral Density');
+            subplot(2, 4, 6);
+            plot(fLvl, 10*log10(pLvl));
+            xlabel('Hz'); ylabel('dB/Hz');
+            title('Level Meter Power Spectral Density');
+            subplot(2, 4, 7);
+            plot(fNorm, 10*log10(pNorm));
+            xlabel('Hz'); ylabel('dB/Hz');
+            title('Normalized Power Spectral Density');
         end
 
-        function builtSignal = buildPSDSig(obj)
-            hello
+        function builtSignal = buildPSDSig(obj, volt)
+            t = 0:1/obj.sampFreq:1-1/obj.sampFreq;
+            builtSignal = zeros([1, length(t)]);
+            for i = 1:obj.numFreqs
+                builtSignal = builtSignal + volt(i)*sin(2*pi*obj.linFreqArray(i)*t);
+%                 x = sin(2*pi*150*t) + randn(size(t));
+            end
         end
 
 
-        function micVoltAvg = avgMicVolt(obj)
+        function [logMicVoltAvg, linMicVoltAvg] = avgMicVolt(obj)
             % Parses and averages the many recorded voltages for each
             % frequency and condenses the averaged values into one 1D
             % voltage array.
 
-            if (strcmp(spacing, 'Log'))
-                freqArray = obj.logFreqArray;
-            else
-                freqArray = obj.linFreqArray;
-            end
-
-            load(obj.micVoltFile);
-            freqArrayStr = string(freqArray);
-            % micVoltArray50 = MicProgramDataParser(a_50);
-            % micVoltArray63 = MicProgramDataParser(a_63);
-            % micVoltArray79 = MicProgramDataParser(a_79); ...
-            for i = 1:length(freqArrayStr)
-                micVoltArray = genvarname(['micVoltArray', char(freqArrayStr(i))]);
-                str = sprintf(' = MicProgramDataParser(obj, a_%d);', freqArray(i));
-                eval([micVoltArray, str]);
+            load(obj.dataFile);
+            logFreqArrayStr = string(obj.logFreqArray);
+            linFreqArrayStr = string(obj.linFreqArray);
+            % logMicVoltArray50 = MicProgramDataParser(fr_50);
+            % logMicVoltArray63 = MicProgramDataParser(fr_63);
+            % logMicVoltArray79 = MicProgramDataParser(fr_79); ...
+            for i = 1:obj.numFreqs
+                logMicVoltArray = genvarname(['logMicVoltArray', char(logFreqArrayStr(i))]);
+                linMicVoltArray = genvarname(['linMicVoltArray', char(linFreqArrayStr(i))]);
+                logStr = sprintf(' = MicProgramDataParser(obj, fr_%d);', obj.logFreqArray(i));
+                linStr = sprintf(' = MicProgramDataParser(obj, psd_%d);', obj.linFreqArray(i));
+                eval([logMicVoltArray, logStr]);
+                eval([linMicVoltArray, linStr]);
             end
             
-            % micVoltAvg50 = sum(micVoltArray50)/length(micVoltArray50);
-            % micVoltAvg63 = sum(micVoltArray63)/length(micVoltArray63);
-            % micVoltAvg79 = sum(micVoltArray79)/length(micVoltArray79); ...
-            for i = 1:length(freqArrayStr)
-                micVoltAvg = genvarname(['micVoltAvg', char(freqArrayStr(i))]);
-                str = sprintf(' = sum(micVoltArray%d)/length(micVoltArray%d);', freqArray(i), freqArray(i));
-                eval([micVoltAvg, str]);
+            % logMicVoltAvg50 = sum(logMicVoltArray50)/length(logMicVoltArray50);
+            % logMicVoltAvg63 = sum(logMicVoltArray63)/length(logMicVoltArray63);
+            % logMicVoltAvg79 = sum(logMicVoltArray79)/length(logMicVoltArray79); ...
+            for i = 1:obj.numFreqs
+                logMicVoltAvg = genvarname(['logMicVoltAvg', char(logFreqArrayStr(i))]);
+                linMicVoltAvg = genvarname(['linMicVoltAvg', char(linFreqArrayStr(i))]);
+                logStr = sprintf(' = sum(logMicVoltArray%d)/length(logMicVoltArray%d);', ...
+                    obj.logFreqArray(i), obj.logFreqArray(i));
+                linStr = sprintf(' = sum(linMicVoltArray%d)/length(linMicVoltArray%d);', ...
+                    obj.linFreqArray(i), obj.linFreqArray(i));
+                eval([logMicVoltAvg, logStr]);
+                eval([linMicVoltAvg, linStr]);
             end
         
-            % micVoltAvg = [micVoltAvg50, micVoltAvg63, micVoltAvg79, ...
-            micVoltAvg = zeros([1, length(freqArrayStr)]);
-            for i = 1:length(micVoltAvg)
-                micVoltA = genvarname(['micVoltAvg', char(freqArrayStr(i))]);
-                str1 = sprintf(['micVoltAvg(i) = ', micVoltA, ';']);
-                eval(str1);
+            % logMicVoltAvg = [logMicVoltAvg50, logMicVoltAvg63, logMicVoltAvg79, ...
+            logMicVoltAvg = zeros([1, obj.numFreqs]);
+            linMicVoltAvg = zeros([1, obj.numFreqs]);
+            for i = 1:obj.numFreqs
+                logMicVoltA = genvarname(['logMicVoltAvg', char(logFreqArrayStr(i))]);
+                linMicVoltA = genvarname(['linMicVoltAvg', char(linFreqArrayStr(i))]);
+                logStr = sprintf(['logMicVoltAvg(i) = ', logMicVoltA, ';']);
+                linStr = sprintf(['linMicVoltAvg(i) = ', linMicVoltA, ';']);
+                eval(logStr);
+                eval(linStr);
             end
         end
 
-        function dataArray = MicProgramDataParser(~, str)
+        function dataArray = MicProgramDataParser(~, strArray)
             % Parses the data obtained via the automated program in the
             % Data Retrieval folder.
 
-            % Typical audio analyzer data string looks like this:
+            % Typical audio analyzer data string array looks like this:
             % ["+01479E-05", "+01484E-05", ... "+01493E-05"]
-            L = length(str);
+            L = length(strArray);
             dataArray = zeros([1, L]);
             for i = 1:L
-                dataArray(i) = str2double(str(i));
+                dataArray(i) = str2double(strArray(i));
             end
         end
 
@@ -248,7 +240,7 @@ classdef Mic
             % folder was used, this function is not needed.
 
             % Typical audio analyzer data string looks like this:
-            % +01479E-05+01484E-05 ... +01493E-05+014
+            % "+01479E-05+01484E-05 ... +01493E-05+014"
             
             % Cuts off last voltage value where recording timeout happens
             % and there is not enough time for it to store all the
@@ -266,50 +258,54 @@ classdef Mic
             end
         end
 
-        function lvlVolt = dbsplToVolt(obj, spacing)
+        function [logLvlVolt, linLvlVolt] = dbsplToVolt(obj)
             % Converts the DBspl gain from the level meter to voltage. The
             % level meter mic sensitivity is needed to properly calculate
             % this.
 
-            if (strcmp(spacing, 'Log'))
-                lvlDBspl = obj.lvlDBsplFR;
-            else
-                lvlDBspl = obj.lvlDBsplPSD;
-            end
-
             % Calculations reference:
             % https://electronics.stackexchange.com/questions/96205/how-to-convert-volts-to-db-spl
-            lvlVolt = zeros([1, length(lvlDBspl)]);
-            for i = 1:length(lvlDBspl)
+            logLvlVolt = zeros([1, length(obj.lvlDBsplFR)]);
+            linLvlVolt = zeros([1, length(obj.lvlDBsplPSD)]);
+            for i = 1:obj.numFreqs
                 % Subtract unit Pascal, 1 Pa = 94 dB.
-                lvlVolt(i) = lvlDBspl(i) - 94;
+                logLvlVolt(i) = obj.lvlDBsplFR(i) - 94;
+                linLvlVolt(i) = obj.lvlDBsplPSD(i) - 94;
                 % Subtract the level meter microphone sensitivity.
-                lvlVolt(i) = lvlVolt(i) - obj.lvlSensDBV;
+                logLvlVolt(i) = logLvlVolt(i) - obj.lvlSensDBV;
+                linLvlVolt(i) = linLvlVolt(i) - obj.lvlSensDBV;
                 % Convert from dB to voltage gain ratio. 
-                lvlVolt(i) = 10^(lvlVolt(i)/20);
+                logLvlVolt(i) = 10^(logLvlVolt(i)/20);
+                linLvlVolt(i) = 10^(linLvlVolt(i)/20);
                 % Multiply the ratio by the level meter voltage sensitivity
                 % to get actual measured level meter voltage.
-                lvlVolt(i) = lvlVolt(i)*obj.lvlSensVolt;
+                logLvlVolt(i) = logLvlVolt(i)*obj.lvlSensVolt;
+                linLvlVolt(i) = linLvlVolt(i)*obj.lvlSensVolt;
             end
         end
 
-        function lvlDBspl = lvlArrayAvgDB(obj)
+        function [logLvlDBspl, linLvlDBspl] = lvlArrayAvgDB(obj)
             % Takes the retrieved level meter data, makes proper
             % conversions, and averages the data for each frequency.
             % Returns a 1D array of averaged level meter DBspl gain
             % corresponding to each tested frequency.
-            lvlDBCellArray = getLevelMeterData(obj);
-            len = length(lvlDBCellArray);
-            lvlDBspl = zeros([1, len]);
-            for i = 1:len        
+            [logLvlDBCellArray, linLvlDBCellArray] = getLevelMeterData(obj);
+            logLvlDBspl = zeros([1, obj.numFreqs]);
+            linLvlDBspl = zeros([1, obj.numFreqs]);
+            for i = 1:obj.numFreqs        
                 % Cannot linearly average values from a logarithmic scale.
                 % Converting DBspl to power is necessary.
-                gainRatio = 10.^(lvlDBCellArray{i}/10);
-                power = gainRatio * 10^(-12);
-                avgPower = sum(power)/length(power);
+                logGainRatio = 10.^(logLvlDBCellArray{i}/10);
+                linGainRatio = 10.^(linLvlDBCellArray{i}/10);
+                logPower = logGainRatio * 10^(-12);
+                linPower = linGainRatio * 10^(-12);
+                logAvgPower = sum(logPower)/length(logPower);
+                linAvgPower = sum(linPower)/length(linPower);
                 % Convert back to DBspl.
-                gainRatio = avgPower / (10^(-12));
-                lvlDBspl(i) = 10*log10(gainRatio);
+                logGainRatio = logAvgPower / (10^(-12));
+                linGainRatio = linAvgPower / (10^(-12));
+                logLvlDBspl(i) = 10*log10(logGainRatio);
+                linLvlDBspl(i) = 10*log10(linGainRatio);
             end
         end
 
