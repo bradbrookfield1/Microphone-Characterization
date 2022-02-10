866e15f8226794644fa1ee1f5534db4a9f2171ec
Brad Brookfield <bradbrookfield1@gmail.com>
2022-02-10 08:32:48 -0600

    Statisfied compiler FIXME

--- Data Visualization/Log Level Meter Files/LevelMeterDataParser.m
+++ Data Visualization/Log Level Meter Files/LevelMeterDataParser.m
@@ -5,11 +5,11 @@ function dataArray = LevelMeterDataParser(str)
     else
         initOffset = 56;
     end
-    startOfData = initOffset + 24;
+    startOfData = initOffset + 23;
     numSamples = int64((length(str) - (initOffset + 2)) / 33);
     dataArray = zeros([1, numSamples]);
     for i = 1:numSamples
         dataArray(i) = str2double(str(startOfData:(startOfData+4)));
-        startOfData = startOfData + 33;
+        startOfData = startOfData + 32;
     end
 end
